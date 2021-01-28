de36891683a244dd48ee82b4b71313e82ee49b0e
egonzalez5 <eduardo.gonzalez5599@gmail.com>
2021-01-28 11:19:08 -0300

    Agrega modal

---
+++ src/context/ModalContext.jsx
@@ -0,0 +1,40 @@
+import React, {createContext, useEffect, useState} from 'react';
+import axios from 'axios';
+
+
+export const ModalContext = createContext();
+
+const ModalProvider = (props) => {
+
+    //state provider
+    const [idReceta, setIdReceta] = useState(null);
+    const [informacion, setReceta] = useState({});
+
+    //llamar api
+    useEffect(() => {
+        const obtenerReceta = async () => {
+            if(!idReceta) return;
+            const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idReceta}`;
+
+            const resultado = await axios.get(url);
+
+            setReceta(resultado.data.drinks[0])
+        }
+        obtenerReceta();
+    }, [idReceta])
+
+    return (
+        <ModalContext.Provider
+            value={{
+                informacion,
+                setIdReceta,
+                setReceta
+            }}
+        >   
+            {props.children}
+        </ModalContext.Provider>
+
+      );
+}
+ 
+export default ModalProvider;
